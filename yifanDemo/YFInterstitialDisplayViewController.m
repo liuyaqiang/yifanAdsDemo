69b71577f81469c737f430cd3111626ef1b99b7d
刘亚强 <liuyaqiang@chargerlink.com>
2017-05-26 10:07:58 +0800

    #

--- yifanDemo/YFInterstitialDisplayViewController.m
+++ yifanDemo/YFInterstitialDisplayViewController.m
@@ -10,16 +10,14 @@
 #import "PluginSDK/PluginHelperOC.h"
 #import "MBProgressHUD+EV.h"
 #import "Masonry.h"
-#import <InMobiSDK/InMobiSDK.h>
 #define PAGE @"main"
 
-@interface YFInterstitialDisplayViewController ()<UITableViewDelegate, UITableViewDataSource,IMInterstitialDelegate>
+@interface YFInterstitialDisplayViewController ()<UITableViewDelegate, UITableViewDataSource>
 {
     NSString *interstitial,*posIntAhead, *posIntAfter;
 }
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) NSArray *dataArr;
-@property (nonatomic, strong) IMInterstitial *interstitialaa;
 
 @end
 
@@ -136,11 +134,7 @@
 {
     [PluginHelperOC hideBanner];
     [PluginHelperOC hideIcon];
-    [PluginHelperOC hideNative];
-    if (self.interstitialaa.isReady) {
-        
-    }
-    
+    [PluginHelperOC hideNative];    
 }
 
 #pragma mark - Get
@@ -162,55 +156,6 @@
         _dataArr = @[@[ interstitial,posIntAhead,posIntAfter]];
     }
     return _dataArr;
-}
-
-/*Indicates that the interstitial is ready to be shown */
-- (void)interstitialDidFinishLoading:(IMInterstitial *)interstitial {
-    NSLog(@"interstitialDidFinishLoading");
-    [self.interstitialaa showFromViewController:self withAnimation:kIMInterstitialAnimationTypeCoverVertical];
-    
-}
-/* Indicates that the interstitial has failed to receive an ad. */
-- (void)interstitial:(IMInterstitial *)interstitial didFailToLoadWithError:(IMRequestStatus *)error {
-    NSLog(@"Interstitial failed to load ad");
-    NSLog(@"Error : %@",error.description);
-}
-/* Indicates that the interstitial has failed to present itself. */
-- (void)interstitial:(IMInterstitial *)interstitial didFailToPresentWithError:(IMRequestStatus *)error {
-    NSLog(@"Interstitial didFailToPresentWithError");
-    NSLog(@"Error : %@",error.description);
-}
-/* indicates that the interstitial is going to present itself. */
-- (void)interstitialWillPresent:(IMInterstitial *)interstitial {
-    NSLog(@"interstitialWillPresent");
-}
-/* Indicates that the interstitial has presented itself */
-- (void)interstitialDidPresent:(IMInterstitial *)interstitial {
-    NSLog(@"interstitialDidPresent");
-}
-/* Indicates that the interstitial is going to dismiss itself. */
-- (void)interstitialWillDismiss:(IMInterstitial *)interstitial {
-    NSLog(@"interstitialWillDismiss");
-}
-/* Indicates that the interstitial has dismissed itself. */
-- (void)interstitialDidDismiss:(IMInterstitial *)interstitial {
-    NSLog(@"interstitialDidDismiss");
-}
-/* Indicates that the user will leave the app. */
-- (void)userWillLeaveApplicationFromInterstitial:(IMInterstitial *)interstitial {
-    NSLog(@"userWillLeaveApplicationFromInterstitial");
-}
-/* Indicates that a reward action is completed */
-- (void)interstitial:(IMInterstitial *)interstitial rewardActionCompletedWithRewards:(NSDictionary *)rewards {
-    NSLog(@"rewardActionCompletedWithRewards");
-}
-/* interstitial:didInteractWithParams: Indicates that the interstitial was interacted with. */
-- (void)interstitial:(IMInterstitial *)interstitial didInteractWithParams:(NSDictionary *)params {
-    NSLog(@"InterstitialDidInteractWithParams");
-}
-/*Not used for direct integration. Notifies the delegate that the ad server has returned an ad but assets are not yet available. */
-- (void)interstitialDidReceiveAd:(IMInterstitial *)interstitial {
-    NSLog(@"interstitialDidReceiveAd");
 }
 
 @end
